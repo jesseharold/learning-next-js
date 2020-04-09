65411886645e88d271f6542dd0b22c44e21e6891
jesseharold <jesseharold@gmail.com>
2020-04-08 22:42:46 -0400

    more show info

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,27 @@
 import Layout from '../../components/BasicLayout';
 import fetch from 'isomorphic-unfetch';
 
-const Post = props => (
+const Post = props => {
+    const { show } = props;
+    if (!show) {
+        return null;
+    }
+    const officialSiteLink = <p><a href={show.officialSite} target='_blank'>Official Site</a></p>;
+    const premiere = <p>Premiered: {show.premiered}</p>;
+    const rating = <p>Rating: {show.rating.average}</p>;
+    const imdbLink = <a href={`https://www.imdb.com/title/${show.externals.imdb}/`} target='_blank'>imdb page</a>;
+return (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-    {props.show.image ? <img src={props.show.image.medium} /> : null}
+    <h1>{show.name}</h1>
+    {show.officialSite ? officialSiteLink : null}
+    {show.externals && show.externals.imdb ? imdbLink : null}
+    {show.premiered ? premiere : null}
+    {show.rating.average ? rating : null}
+    <p>{show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    {show.image ? <img src={show.image.medium} /> : null}
   </Layout>
 );
+}
 
 Post.getInitialProps = async function(context) {
   const { id } = context.query;
